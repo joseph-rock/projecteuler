1e87ad1f3425254fd93a87d4c565c860fe935b0b
Joseph <josephrock225@gmail.com>
2022-08-20 12:24:46 -0700

    Init repo

---
+++ p03_LargestPrimeFactor.ts
@@ -0,0 +1,40 @@
+/**
+ * The prime factors of 13195 are 5, 7, 13 and 29.
+ *
+ * What is the largest prime factor of the number 600851475143 ?
+ */
+
+function isPrime(num: number): boolean {
+  for (let i = 2; i < num; i++) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function primeFactor(num: number): number {
+  const truncatedNum: number = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= truncatedNum; i++) {
+    if (isPrime(i) && num % i == 0) {
+      return i;
+    }
+  }
+  return 0;
+}
+
+function factorTree(seed: number): number[] {
+  const factors: number[] = [];
+
+  while (primeFactor(seed) > 0) {
+    factors.push(primeFactor(seed));
+    seed = seed / primeFactor(seed);
+  }
+  factors.push(seed);
+
+  return factors;
+}
+
+const target = 600851475143;
+console.log(target);
+console.log(factorTree(target));
---
+++ p04_LargestPalindromeProduct.ts
@@ -0,0 +1,43 @@
+/**
+ * A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is
+ * 9009 = 91 × 99.
+ *
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ */
+
+function isPalindrome(num: number): boolean {
+  const numString: string[] = num.toString().split("");
+  const revString: string[] = numString;
+  return numString.toString() == revString.reverse().toString();
+}
+
+function isThreeDigits(num: number): boolean {
+  return num > 99 && num < 1000;
+}
+
+function nextLargestPalindrome(num: number): number {
+  do {
+    num--;
+  } while (!isPalindrome(num));
+
+  return num;
+}
+
+function findFactors(num: number): [number, number] {
+  if (!isPalindrome(num)) {
+    num = nextLargestPalindrome(num);
+  }
+
+  for (let i = 999; i > 99; i--) {
+    if (num % i == 0 && isThreeDigits(num / i)) {
+      return [i, num / i];
+    }
+  }
+
+  return findFactors(num - 1);
+}
+
+const largestNum = 999 * 999;
+const ans: [number, number] = findFactors(largestNum);
+console.log(ans[0] * ans[1]);
+console.log(ans);
