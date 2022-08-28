ec76248cd949b36694732e4e21d7c63a93a3aab9
Joseph <josephrock225@gmail.com>
2022-08-28 14:24:30 -0700

    Init

---
+++ p10_SummationOfPrimes.ts
@@ -0,0 +1,33 @@
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
+function isPrime(num: number): boolean {
+  const truncatedNum: number = Math.floor(Math.sqrt(num));
+  for (let i = 2; i <= truncatedNum; i++) {
+    if (num % i == 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function nextPrime(num: number): number {
+  if (!isPrime(num + 1)) {
+    return nextPrime(num + 1);
+  }
+  return num + 1;
+}
+
+function primes(): number[] {
+  const primes: number[] = [2];
+  while (primes[primes.length - 1] < 2000000) {
+    primes.push(nextPrime(primes[primes.length - 1]));
+  }
+  primes.pop();
+  return primes;
+}
+
+console.log(primes().reduce((acc, e) => acc + e));
